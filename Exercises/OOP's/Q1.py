517812d86064b5ecba5346152b1c848e2466ea68
Jaswanth-Kumar-2007 <jaswanthkumarkamireddi@gmail.com>
2025-10-16 18:28:08 +0530

    Added OOP's

---
+++ Exercises/OOP's/Q1.py
@@ -0,0 +1,57 @@
+# Base Class
+class Shape():
+    def __init__(self,color,thickness):
+        self.color = color
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+# Triangle Class
+class Triangle(Shape):
+    def __init__(self,color,thickness,base,height,side1,side2,side3):
+        super().__init__(color,thickness)
+        self.base = base
+        self.height = height
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+    def area(self):
+        print(0.5 * self.base * self.height)
+
+    def perimeter(self):
+        print(self.side1 + self.side2 + self.side3)
+
+
+# Circle class
+class Circle(Shape):
+    def __init__(self, color, border_thickness, radius):
+        super().__init__(color, border_thickness)
+        self.radius = radius
+
+    def area(self):
+        print((22/7) * self.radius ** 2)
+
+    def perimeter(self):
+        print(2 * (22/7) * self.radius)
+
+
+# Rectangle class
+class Rectangle(Shape):
+    def __init__(self, color, border_thickness, length, width):
+        super().__init__(color, border_thickness)
+        self.length = length
+        self.width = width
+
+    def area(self):
+        print(self.length * self.width)
+
+    def perimeter(self):
+        print(2 * (self.length + self.width))
+
+rectangle1 = Rectangle("White",5,2,3)
+
+rectangle1.area()
---
+++ Exercises/OOP's/Q2.py
@@ -0,0 +1,23 @@
+# Movie Class 
+class Movie():
+    def __init__(self,title,genre,rating,year):
+        self.title = title
+        self.genre = genre
+        self.rating = rating
+        self.year = year
+
+    def display_info(self):
+        print(f"Title --> {self.title}")
+        print(f"Genre --> {self.genre}")
+        print(f"Rating --> {self.rating}")
+        print(f"Year --> {self.year}")
+
+    def update_rating(self,new_rating):
+        self.rating = new_rating
+        print(f"Updated Rating is {self.rating}")
+
+movie1 = Movie("Doctor","Comedy",10.0,2022)
+
+movie1.display_info()
+movie1.update_rating(9.5)
+movie1.display_info()
