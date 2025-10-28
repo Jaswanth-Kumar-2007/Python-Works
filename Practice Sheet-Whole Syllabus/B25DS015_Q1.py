713fb15197d530b06416ef5e15830e490bc507b3
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2025-10-28 07:17:00 +0530

    Add files via upload

---
+++ Practice Sheet-Whole Syllabus/B25DS015_Q1.py
@@ -0,0 +1,9 @@
+def FizzBuzz(n):
+    res = ["FizzBuzz"  if i %3 == 0 and i % 5 == 0
+           else "Fizz" if  i% 3 == 0
+           else "Buzz" if i % 5 == 0
+           else str(i)
+           for i in range(1,n+1)]
+    return res
+
+print(FizzBuzz(5))
