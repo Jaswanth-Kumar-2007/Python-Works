54aa6ca36045ab1868f02906968406f53b295f6f
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2025-11-22 15:02:48 +0530

    Added Exercises

---
+++ Exercises/FunctionsinPy/global_static.py
@@ -0,0 +1,24 @@
+s = 45
+
+def add():
+    s = 5
+    s += 10
+    print(s)
+
+add()
+print(s)
+
+# 15 , 45
+
+global p
+p = 20
+
+def addi():
+    p = 5
+    p += 10
+    return p
+
+print(addi())
+print(p)
+
+#
---
+++ Exercises/List,Set,Dict-Comprehensions/Slicing.py
@@ -0,0 +1,15 @@
+p = ['a','b','c','d','e','f']
+
+print(p[-1:-5])
+
+# []
+
+print(p[-1:-5:-1])
+
+# ['f','e','d','c']
+
+p.insert(2,'h')
+p.append('h')
+
+print(p)
+
